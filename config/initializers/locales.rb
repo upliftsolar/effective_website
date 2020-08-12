19019a77111e3dddc84107dd280f8144848cb973
Darius Roberts <darius.roberts@gmail.com>
2020-08-12 08:42:51 -0700

    add config

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -3,7 +3,7 @@
 I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
  
 # Permitted locales available for the application
-I18n.available_locales = [:en, :sp]
+I18n.available_locales = [:en, :es]
  
 # Set default locale to something other than :en
-I18n.default_locale = :sp
+I18n.default_locale = :es
