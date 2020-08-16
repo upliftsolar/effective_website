e016fb215634977a493e942287d162403b8be6ff
Darius Roberts <darius.roberts@gmail.com>
2020-08-16 14:58:24 -0700

    translate effective pages

--- app/helpers/override_effective_regions_for_translations_helper.rb
+++ app/helpers/override_effective_regions_for_translations_helper.rb
@@ -5,7 +5,7 @@ EffectiveRegionsHelper.module_eval do
   define_method(:effective_region) do |*args|
     r = old_effective_region.bind(self).(*args).dup
     if @locale 
-      lang_tags = r.scan(/(([A-Z]{2}):\[(.*?)\])/m)
+      lang_tags = r.scan(/(([A-Z]{2}):\[\[(.*?)\]\])/m)
       for lang_tag in lang_tags
         if lang_tag[1].to_s == @locale.to_s.upcase
           r.gsub!(lang_tag[0],lang_tag[2])
