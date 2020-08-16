c69386e86cbe1d76c17d63b64557090a15e4cda8
Darius Roberts <darius.roberts@gmail.com>
2020-08-16 16:24:26 -0700

    translations should work for all regions

--- app/helpers/override_effective_regions_for_translations_helper.rb
+++ app/helpers/override_effective_regions_for_translations_helper.rb
@@ -1,21 +1,27 @@
 require 'effective_regions_helper'
 EffectiveRegionsHelper.module_eval do
-  old_effective_region = instance_method(:effective_region)
+  old_effective_region = instance_method(:ckeditor_region)
 
-  define_method(:effective_region) do |*args|
-    r = old_effective_region.bind(self).(*args).dup
-    if @locale 
-      lang_tags = r.scan(/(([A-Z]{2}):\[\[(.*?)\]\])/m)
-      for lang_tag in lang_tags
-        if lang_tag[1].to_s == @locale.to_s.upcase
-          r.gsub!(lang_tag[0],lang_tag[2])
-        else
-          r.gsub!(lang_tag[0],"")
-        end
-      end
+  define_method(:ckeditor_region) do |args,options={},&block|
+    r = old_effective_region.bind(self).(args,options,&block).dup
+    if effectively_editing?
+      #ok
+    elsif r
+      handle_translation(r)
     end
     r.html_safe
   end 
+  def handle_translation(r)
+    lang_tags = r.scan(/(([A-Z]{2}):\[\[(.*?)\]\])/m)
+    for lang_tag in lang_tags
+      if lang_tag[1].to_s == @locale.to_s.upcase
+        r.gsub!(lang_tag[0],lang_tag[2])
+      else
+        r.gsub!(lang_tag[0],"")
+      end
+    end
+    r
+  end
 end
 
 OverrideEffectiveRegionsForTranslationsHelper =  EffectiveRegionsHelper
