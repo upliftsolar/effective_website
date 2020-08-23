6263f562b4c75ba27524a94cabc6209e8fffc862
Darius Roberts <darius.roberts@gmail.com>
2020-08-23 07:47:43 -0700

    fix redirect and flash for Lead generation

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -2,10 +2,15 @@ class LeadsController < ApplicationController
   include Effective::CrudController
   page_title ::I18n.t("lead page"), only: [:new]
 
-  def create
-    r = super
-    flash[:notice] = I18n.t(:success_lead_submission)
-    #Redirect to?
-    r
+  def resource_redirect_path(action)
+    root_path
+  end
+
+  def resource_flash(status, *args)
+    if status == :success
+      I18n.t(:success_lead_submission)
+    else
+      super
+    end
   end
 end
