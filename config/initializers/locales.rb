2279fed294dd486269388d7b0f85e2733766e32a
Darius Roberts <darius.roberts@gmail.com>
2020-08-23 21:23:10 -0700

    enable eo for homepage-only translation

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -3,7 +3,7 @@
 I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
  
 # Permitted locales available for the application
-I18n.available_locales = [:en, :es]
+I18n.available_locales = [:en, :es, :eo]
  
 # Set default locale to something other than :en
 if ENV['DEFAULT_LOCALE']
