a720757b4ad55481fd6f99b6d46a5b039bebc62d
Darius Roberts <darius.roberts@gmail.com>
2020-08-29 13:29:54 -0700

    add font-awesom and fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,6 @@
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "stylesheets","fonts","HaasGrotDisp-35Thin")
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "stylesheets","fonts","HaasGrotDisp-45Light")
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "stylesheets","fonts","HaasGrotDisp-65Medium")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "stylesheets","fonts","Apercu")
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 Rails.application.config.assets.precompile += %w( barrio.css uplift.css )
