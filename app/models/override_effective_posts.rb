37b2dcb7c3e987dee46157f6ba9d86eb56f67a53
Darius Roberts <darius.roberts@gmail.com>
2020-09-23 08:25:02 -0700

    always use website_href

--- app/models/override_effective_posts.rb
+++ app/models/override_effective_posts.rb
@@ -1,6 +1,8 @@
 require 'effective_posts'
 class ::Effective::Post
   has_many_attached :attachments
+  #TODO: figure out how to make fields show up on 'create'. _additional_fields is a little wonky.
+  #validates :website_href, if: -> { ["news","papers"].include? category }, presence: true
 end
 class OverrideEffectivePosts
   #TODO: use autoloads.
